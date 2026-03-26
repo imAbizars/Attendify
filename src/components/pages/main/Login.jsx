3ce29714c2cb38f4864e3cddd0e90a402b95bf76
Dennis Abizar <denisabizar54@gmail.com>
2026-03-26 09:55:04 +0700

    add : animation login

--- src/components/pages/main/Login.jsx
+++ src/components/pages/main/Login.jsx
@@ -10,6 +10,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Alert,AlertDescription } from "@/components/ui/alert";
+import Lottie from "lottie-react";
+import SuccessAnimation from "@/assets/animation/Successfull Animation.json";
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
@@ -21,7 +24,8 @@ export default function Login(){
     const [error, setError] = useState("");
     const [loading,setLoading] = useState(false);
     const navigate = useNavigate();
-
+    const [alertSuccess,setAlertSuccess] = useState(false);
+    const [userRole, setUserRole] = useState("");
     const handleLogin = async (e) => {
     e.preventDefault();
     setError(""); 
@@ -30,17 +34,13 @@ export default function Login(){
     if (!email) return setError("Email tidak boleh kosong");
     if (!password) return setError("Password tidak boleh kosong");
     if (!/\S+@\S+\.\S+/.test(email)) return setError("Format email tidak valid");
-    if (password.length < 6) return setError("Password minimal 6 karakter");
 
     setLoading(true); 
     try {
         const payload = await authLogin(email, password);
-
-        if (payload.role === "ADMIN") {
-            navigate("/dashboard");
-        } else {
-            navigate("/home");
-        }
+        setUserRole(payload.role);
+        setAlertSuccess(true);
+        
     } catch (err) {
         const message = err.response?.data?.message;
         if (message === "Email tidak terdaftar") {
@@ -102,6 +102,27 @@ export default function Login(){
                     </Button>
                 </CardFooter>
             </Card>
+            {/* todo : add animation login */}
+            {alertSuccess && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+                    <Alert className="w-50 h-50 flex items-center justify-center bg-white">
+                        <AlertDescription className="flex flex-col items-center justify-center text-xl">
+                            <Lottie
+                            animationData={SuccessAnimation}
+                            loop={false}
+                            style={{width:200,height:200}}
+                            onComplete={() => {
+                                if (userRole === "ADMIN") {
+                                    navigate("/dashboard");
+                                } else {
+                                    navigate("/home");
+                                }
+                            }}
+                            />
+                        </AlertDescription>
+                    </Alert>
+                </div>
+            )}
         </div>
     )
 }
