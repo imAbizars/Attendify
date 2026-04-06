2e72f0405092438c384a0e08d42ad18321858129
Dennis Abizar <denisabizar54@gmail.com>
2026-04-06 17:38:00 +0700

    fixing url api

--- src/lib/axios/axios.js
+++ src/lib/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://rest-api-attendify-mu4m-eesyxnkj9-gyozasushis-projects.vercel.app/"
+    baseURL:"https://rest-api-attendify.vercel.app/"
 })
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
